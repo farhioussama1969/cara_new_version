5354edebae831dfc194cf47a0c000a300b19b56c
Farhi oussama baha eddine <farhioussama1969@gmail.com>
2024-08-31 23:46:40 +0100

    my reservation screen screen finished

--- lib/app/modules/gifts/controllers/gifts_controller.dart
+++ lib/app/modules/gifts/controllers/gifts_controller.dart
@@ -1,11 +1,43 @@
 import 'package:get/get.dart';
+import 'package:solvodev_mobile_structure/app/core/constants/get_builders_ids_constants.dart';
+import 'package:solvodev_mobile_structure/app/data/models/free_washing_config_model.dart';
+import 'package:solvodev_mobile_structure/app/data/providers/cara_api/config_provider.dart';
+import 'package:solvodev_mobile_structure/app/modules/home/controllers/home_controller.dart';
 
 class GiftsController extends GetxController {
-  //TODO: Implement GiftsController
+  FreeWashingConfigModel? freeWashingConfig;
 
-  final count = 0.obs;
+  void changeFreeWashingConfig(FreeWashingConfigModel? value) {
+    freeWashingConfig = value;
+    update([GetBuildersIdsConstants.homePaymentWindow]);
+  }
+
+  bool getFreeWashingConfigLoading = false;
+  void changeGetFreeWashingConfigLoading(bool value) {
+    getFreeWashingConfigLoading = value;
+    update([GetBuildersIdsConstants.homePaymentWindow]);
+  }
+
+  void getFreeWashingConfig() {
+    ConfigProvider()
+        .freeWashingConfig(
+      branchId: Get.find<HomeController>()
+          .checkServiceAvailabilityResponse
+          ?.branch
+          ?.id,
+      onLoading: () => changeGetFreeWashingConfigLoading(true),
+      onFinal: () => changeGetFreeWashingConfigLoading(false),
+    )
+        .then((value) {
+      if (value != null) {
+        changeFreeWashingConfig(value);
+      }
+    });
+  }
+
   @override
   void onInit() {
+    getFreeWashingConfig();
     super.onInit();
   }
 
@@ -18,6 +50,4 @@ class GiftsController extends GetxController {
   void onClose() {
     super.onClose();
   }
-
-  void increment() => count.value++;
 }
